d6f12788c7bced5e32d38d7b843b9a93aed8e8ee
farhanintesar <farhanintesar0099@gmail.com>
2025-10-10 00:38:30 +0600

    ok  now done

--- src/components/StepsSection.jsx
+++ src/components/StepsSection.jsx
@@ -42,50 +42,49 @@ export function StepsSection() {
     <section id="you" className="px-4 w-auto lg:w-11/12 mx-auto ">
       {/* Top Section */}
       <section className="flex items-center justify-center my-5 md:my-16">
-        <div className="w-full  mx-auto text-center">
-          <div className="flex items-center justify-start gap-2 mb-16">
+        <div className="w-full mx-auto text-left md:text-center px-4">
+          <div className="flex items-center justify-start gap-2 mb-5 md:mb-16">
             <CirclePlus className="w-6 h-6 text-black font-bold" />
-            <span className="text-md md:text-xl font-semibold text-black">
+            <span className="text-lg md:text-xl font-semibold text-black">
               What We Do
             </span>
           </div>
 
           {/* Main heading */}
           <motion.h1
-            className="text-2xl md:text-3xl lg:text-6xl font-[500] text-balance leading-tight"
+            className="text-3xl sm:text-4xl md:text-3xl lg:text-6xl font-[500] text-balance leading-tight"
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
+            transition={{ duration: 0.8, ease: 'easeOut' }}
             viewport={{ once: true, amount: 0.3 }}
           >
             <span className="text-black">You’ve built the brand.</span>
             <br />
             <span className="text-[#686868]">
-              {" "}
-              We make it resonate with precision.{" "}
+              We make it resonate with precision.
             </span>
           </motion.h1>
         </div>
       </section>
 
-      <section className="flex items-center justify-center mt-10">
-        <div className="w-full  mx-auto text-center my-6">
+      <section className="flex items-center justify-center mt-5 md:mt-10">
+        <div className="w-full mx-auto text-left md:text-center my-6 px-4">
           {/* Main heading */}
           <motion.h1
-            className="text-xl mb-10 "
+            className="text-lg sm:text-xl md:text-xl mb-5 md:mg-10"
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
+            transition={{ duration: 0.8, ease: 'easeOut' }}
             viewport={{ once: true, amount: 0.3 }}
           >
             <span className="text-black">
               Our services are delivered through five strategic pillars. <br />
-              These are the ways we partner with you to build, grow, and sustain
-              your brand.
+              These are the ways we partner with you to build, grow, and sustain your brand.
             </span>
           </motion.h1>
         </div>
       </section>
+
       {/* Steps Grid */}
 
       <div className="">
@@ -93,7 +92,8 @@ export function StepsSection() {
           {steps.map((step, index) => (
             <motion.div
               key={index}
-              className="bg-white p-3 sm:p-4 rounded-xl text-left hover:shadow-lg transition-shadow duration-300"
+              className="bg-white p-3 sm:p-4 rounded-xl text-left hover:shadow-lg transition-shadow duration-300 py-6
+              "
               initial={{ opacity: 0, y: 30 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{
@@ -113,21 +113,20 @@ export function StepsSection() {
                   {Array.from({ length: 5 }).map((_, dotIndex) => (
                     <div
                       key={dotIndex}
-                      className={`w-1 h-1 sm:w-1.5 sm:h-1.5 rounded-full ${
-                        dotIndex < step.activeDots ? "bg-black" : "bg-gray-300"
-                      }`}
+                      className={`w-1 h-1 sm:w-1.5 sm:h-1.5 rounded-full ${dotIndex < step.activeDots ? "bg-black" : "bg-gray-300"
+                        }`}
                     />
                   ))}
                 </div>
               </div>
 
               {/* Step title */}
-              <h3 className="text-sm sm:text-base md:text-lg font-bold text-[#686868] mb-1">
+              <h3 className="text-lg sm:text-base md:text-lg font-bold text-[#686868] mb-1 px-7 md:px-0">
                 {step.title}
               </h3>
 
               {/* Step text */}
-              <p className="text-xs sm:text-sm md:text-sm leading-snug font-medium text-[#686868]">
+              <p className="text-lg sm:text-sm md:text-sm leading-snug font-medium text-black px-7 md:px-0">
                 {step.text}
               </p>
             </motion.div>
