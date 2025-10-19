73371d5036bce79f93c8f57509a5a77016a58fee
nabishafin <shafin21215@gmail.com>
2025-10-20 02:06:02 +0600

    main page done

--- src/app/page.js
+++ src/app/page.js
@@ -12,63 +12,45 @@ import ClientTestimonialsSection from "@/components/ClientReview";
 import TeamSection from "@/components/TeamSection";
 
 export default function Home() {
-  const [showVideo, setShowVideo] = useState(true);
   useLenis();
 
-  const handleVideoEnd = () => {
-    setShowVideo(false);
-  };
-
   return (
-    <>
-      {showVideo && (
-        <div className="fixed inset-0 z-[10000] bg-black flex items-center justify-center">
-          <video
-            src="logo video.mp4"
-            autoPlay
-            muted
-            playsInline
-            onEnded={handleVideoEnd}
-            className="w-full h-full object-contain"
-          />
-        </div>
-      )}
-      <main className="bg-[#F5F5F5]">
-        <section id="hero">
-          <HeroSection />
-        </section>
+    <main className="bg-[#F5F5F5]">
+      <section id="hero">
+        <HeroSection />
+      </section>
 
-        <section>
-          <HowWeWorkSection />
-        </section>
+      <section>
+        <HowWeWorkSection />
+      </section>
 
-        <section>
-          <CompanyLogos />
-        </section>
+      <section>
+        <CompanyLogos />
+      </section>
 
-        <section>
-          <TestimonialSection />
-        </section>
+      <section>
+        <TestimonialSection />
+      </section>
 
-        <section>
-          <StepsSection />
-        </section>
-        <section id="reviews">
-          <ClientTestimonialsSection />
-        </section>
+      <section>
+        <StepsSection />
+      </section>
 
-        <section id="us">
-          <TeamSection />
-        </section>
+      <section id="reviews">
+        <ClientTestimonialsSection />
+      </section>
 
-        <section>
-          <FaqSection />
-        </section>
+      <section id="us">
+        <TeamSection />
+      </section>
 
-        <section id="contact">
-          <ContactSection />
-        </section>
-      </main>
-    </>
+      <section>
+        <FaqSection />
+      </section>
+
+      <section id="contact">
+        <ContactSection />
+      </section>
+    </main>
   );
 }
